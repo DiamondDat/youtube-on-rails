05e1a57872f93dbe536b2383456f680890352b48
Dat Tang <dat.tang@deliveree.com>
2020-08-23 02:35:16 +0700

    Add-devise

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: :true, uniqueness: true
+
+  def email_required?
+    false
+  end
+
+  # use this instead of email_changed? for Rails = 5.1.x
+  def will_save_change_to_email?
+    false
+  end
+end
